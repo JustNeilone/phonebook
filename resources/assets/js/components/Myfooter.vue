77c3c914419b472f263e13d9f8b1f3a7a7547d5c
JustNeilone <egorkozhadey@gmail.com>
2018-06-29 11:59:04 +0400

    Initial Commit

---
+++ resources/assets/js/components/Myfooter.vue
@@ -0,0 +1,10 @@
+<template>
+<footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      <strong>Laravel|Vue.js Phonebook Application</strong> by <a href="https://github.com/JustNeilone">Egor Kozhadey</a>. The source code can be found at
+      <a href="https://github.com/JustNeilone/phonebook">github.com/JustNeilone/phobebook</a>.
+    </p>
+  </div>
+</footer>
+</template>
---
+++ resources/assets/js/components/Myheader.vue
@@ -0,0 +1,27 @@
+<template>
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://localhost:8000">
+      <img src="phonebook.jpg" alt="Egor's Phonebook" width="140" height="28">
+    </a>
+
+
+    <div class="navbar-menu is-active">
+  <a class="navbar-item" href="">
+      Home
+    </a>
+
+    <a class="navbar-item" href="">
+      About
+    </a>
+    </div>
+    
+
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+</nav>
+</template>
